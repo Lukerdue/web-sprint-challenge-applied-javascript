2497a0612a72de253824148bd3b94ccbe07245fd
Jamie <jamielynjenks@gmail.com>
2020-12-04 09:22:52 -0700

    created tabs component and topic axios request

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,24 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+axios.get("https://lambda-times-api.herokuapp.com/topics")
+    .then(res=>{
+        console.log(res.data.topics)
+        res.data.topics.forEach(t=>{
+            let tab = tabMaker(t);
+            document.querySelector('.topics').appendChild(tab);
+        })
+    })
+    .catch(drama=>{
+        console.log(drama)
+    })
+
+function tabMaker(topic){
+    let tab = document.createElement('div');
+
+    tab.classList.add('tab');
+    tab.textContent = topic;
+
+    return tab;
+}
+
